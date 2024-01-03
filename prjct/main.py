a2c7293bf76c274521bc1aa4648799350572ed9c
darysun07 <sockolenkodar@yandex.ru>
2024-01-03 20:42:52 +0300

    hero3

--- prjct/main.py
+++ prjct/main.py
@@ -1,178 +1,155 @@
-import sys
 import pygame
-from PyQt5 import uic, QtWidgets
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtSql import QSqlTableModel
+from pygame import mixer
+from fighter import Fighter
 
-import sqlite3
-
-
-class LoginDb:
-    def __init__(self, dbname):
-        self.dbname = dbname
-        self.conn = sqlite3.connect(dbname)
-
-    def is_table(self, table_name):
-        query = "SELECT name from sqlite_master WHERE type='table' AND name='{}';".format(table_name)
-        cursor = self.conn.execute(query)
-        result = cursor.fetchone()
-        if result is None:
-            return False
-        else:
-            return True
-
-
-class Home(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('wild_west.ui', self)
-
-        self.loginDb = LoginDb('login.db')
-        if self.loginDb.is_table('USERS'):
-            pass
-        else:
-            self.loginDb.conn.execute("CREATE TABLE USERS(NAME TEXT NOT NULL,PASSWORD TEXT)")
-            self.loginDb.conn.execute("INSERT INTO USERS VALUES(?, ?)",
-                                            ('admin', 'admin'))
-            self.loginDb.conn.commit()
-        self.enter_btn.clicked.connect(self.loginCheck)
-        self.signup_btn.clicked.connect(self.signup_window_opn)
-        self.pravila.clicked.connect(self.pravila_window_opn)
-
-    def glav_window_opn(self):
-        self.glav_window = Fon()
-        self.glav_window.show()
-
-    def signup_window_opn(self):
-        self.signup_window_opn = Signup()
-        self.signup_window_opn.show()
-        self.close()
-
-    def pravila_window_opn(self):
-        self.pravila_window_opn = Pravila()
-        self.pravila_window_opn.show()
-        self.close()
-
-    def loginCheck(self):
-        name = self.name_check.text()
-        password = self.password_check.text()
-        if (not name) or (not password):
-            err_msg = QMessageBox.information(self, 'Внимание!', 'Не все поля заполнены.')
-            return err_msg
-
-        rslt = self.loginDb.conn.execute("SELECT * FROM USERS WHERE NAME = ? AND PASSWORD = ?", (name, password))
-        if len(rslt.fetchall()):
-            self.close()
-            self.glav_window_opn()
-            self.loginDb.conn.close()
-        else:
-            err_msg = QMessageBox.information(self, 'Внимание!', 'Неправильное имя пользователя или пароль.')
-            return err_msg
-
-
-class Signup(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('login.ui', self)
-        self.reg_btn.clicked.connect(self.insertData)
-
-        self.loginDb = LoginDb('login.db')
-
-    def insertData(self):
-        name = self.name_save.text()
-        password = self.password_save.text()
-
-        if (not name) or (not password):
-            err_msg = QMessageBox.information(self, 'Внимание!', 'Не все поля заполнены.')
-            return err_msg
-
-        rslt = self.loginDb.conn.execute("SELECT * FROM USERS WHERE NAME = ?", (name,))
-
-        if rslt.fetchall():
-            err_msg = QMessageBox.information(self, 'Внимание!', 'Пользоватеть с таким именем уже зарегистрирован.')
-            return err_msg
-
-        else:
-            self.loginDb.conn.execute("INSERT INTO USERS VALUES(?, ?)", (name, password))
-            self.loginDb.conn.commit()
-            self.home_window_opn = Home()
-            self.home_window_opn.show()
-            self.close()
-
-
+mixer.init()
 pygame.init()
 
-
+#create game window
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
-size = SCREEN_WIDTH, SCREEN_HEIGHT
-screen = pygame.display.set_mode(size)
-bg_im = pygame.image.load("fon2.png").convert_alpha()
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
 
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colours
+RED = (255, 15, 192)
+GREEN = (0, 255, 0)
+WHITE = (255, 255, 255)
+
+#define game variables
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+score = [0, 0]#player scores. [P1, P2]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+#define fighter variables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+
+#load music and sounds
+pygame.mixer.music.load("assets/audio/music.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1, 0.0, 5000)
+sword_fx = pygame.mixer.Sound("assets/audio/sword.wav")
+sword_fx.set_volume(0.5)
+magic_fx = pygame.mixer.Sound("assets/audio/magic.wav")
+magic_fx.set_volume(0.75)
+
+#load background image
+bg_image = pygame.image.load("assets/images/background/fon2.png").convert_alpha()
+
+#load spritesheets
+warrior_sheet = pygame.image.load("assets/images/warrior/Sprites/warrior.png").convert_alpha()
+wizard_sheet = pygame.image.load("assets/images/wizard/Sprites/wizard.png").convert_alpha()
+
+#load vicory image
+victory_img = pygame.image.load("assets/images/icons/victory.png").convert_alpha()
+
+#define number of steps in each animation
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+
+#define font
+count_font = pygame.font.Font("assets/fonts/turok.ttf", 80)
+score_font = pygame.font.Font("assets/fonts/turok.ttf", 30)
+
+#function for drawing text
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
+
+#function for drawing background
 def draw_bg():
-  scaled_bg = pygame.transform.scale(bg_im, (SCREEN_WIDTH, SCREEN_HEIGHT))
+  scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
   screen.blit(scaled_bg, (0, 0))
 
-
-class Hero(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.hero = pygame.image.load('hero.png').convert_alpha()
-        self.hero.set_colorkey((255, 255, 255))
-        self.rect_hero = self.hero.get_rect()
-        self.rect_hero.x = x
-        self.rect_hero.y = y
-
-    def draw(self, surface):
-        surface.blit(self.hero, (self.rect_hero.x, self.rect_hero.y))
+#function for drawing fighter health bars
+def draw_health_bar(health, x, y):
+  ratio = health / 100
+  pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 404, 34))
+  pygame.draw.rect(screen, RED, (x, y, 400, 30))
+  pygame.draw.rect(screen, GREEN, (x, y, 400 * ratio, 30))
 
 
-running = True
+#create two instances of fighters
+fighter_1 = Fighter(1, 200, 400, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+fighter_2 = Fighter(2, 850, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+
+#game loop
+run = True
+while run:
+
+  clock.tick(FPS)
+
+  #draw background
+  draw_bg()
+
+  #show player stats
+  draw_health_bar(fighter_1.health, 40, 20)
+  draw_health_bar(fighter_2.health, 700, 20)
+  draw_text("P1: " + str(score[0]), score_font, RED, 40, 60)
+  draw_text("P2: " + str(score[1]), score_font, RED, 700, 60)
+
+  #update countdown
+  if intro_count <= 0:
+    #move fighters
+    fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
+    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+  else:
+    #display count timer
+    draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+    #update count timer
+    if (pygame.time.get_ticks() - last_count_update) >= 1000:
+      intro_count -= 1
+      last_count_update = pygame.time.get_ticks()
+
+  #update fighters
+  fighter_1.update()
+  fighter_2.update()
+
+  #draw fighters
+  fighter_1.draw(screen)
+  fighter_2.draw(screen)
+
+  #check for player defeat
+  if round_over == False:
+    if fighter_1.alive == False:
+      score[1] += 1
+      round_over = True
+      round_over_time = pygame.time.get_ticks()
+    elif fighter_2.alive == False:
+      score[0] += 1
+      round_over = True
+      round_over_time = pygame.time.get_ticks()
+  else:
+    #display victory image
+    screen.blit(victory_img, (360, 150))
+    if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+      round_over = False
+      intro_count = 3
+      fighter_1 = Fighter(1, 200, 400, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+      fighter_2 = Fighter(2, 850, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
 
 
-vel = 5
-jump = False
-jumpCount = 0
-jumpMax = 15
-FPS = 60
-clock = pygame.time.Clock()
+  #update display
+  pygame.display.update()
 
-hero1 = Hero(250, 250)
-
-while running:
-    draw_bg()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            if event.type == pygame.KEYDOWN:
-                if not jump and event.key == pygame.K_SPACE:
-                    jump = True
-                    jumpCount = jumpMax
-        keys = pygame.key.get_pressed()
-        if jump:
-            y_h -= jumpCount
-            if jumpCount > -jumpMax:
-                jumpCount -= 1
-            else:
-                jump = False
-    hero1.update()
-    hero1.draw(screen)
-    clock.tick(FPS)
-    pygame.display.flip()
+#exit pygame
 pygame.quit()
-
-
-class Pravila(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('pravila.ui', self)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    st = Home()
-    st.show()
-    sys.exit(app.exec())
