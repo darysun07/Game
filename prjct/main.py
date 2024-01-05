97f111dfe635953380d041a7d4f010b209b8615c
darysun07 <sockolenkodar@yandex.ru>
2024-01-05 17:57:38 +0300

    3

--- prjct/main.py
+++ prjct/main.py
@@ -10,7 +10,7 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 700
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Brawler")
+pygame.display.set_caption("Wild West")
 
 #set framerate
 clock = pygame.time.Clock()
