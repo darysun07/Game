577b7104df86cf2a2faa9a70b5e09ea28bb55a18
twwixie <booch2019@mail.ru>
2024-01-06 23:42:59 +0300

    nn

--- prjct/main1.py
+++ prjct/main1.py
@@ -410,9 +410,10 @@ class Yrovni(QMainWindow):
                                       sword_fx, 70)
                     player_2 = Player(2, 850, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx, 70)
 
-            if score[0] == 1 or score[1] == 1:
+            if score[0] == 3 or score[1] == 3:
                 game_over_img = pygame.transform.scale(game_over_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
                 screen.blit(game_over_img, (0, 0))
+                pygame.mixer.music.stop()
                 round_over = True
 
             # event handler
@@ -547,7 +548,9 @@ class Yrovni(QMainWindow):
                     player_2 = Player(2, 850, 400, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx, 110)
 
             if score[0] == 3 or score[1] == 3:
-                screen.blit(game_over_img, (500, 300))
+                game_over_img = pygame.transform.scale(game_over_img, (SCREEN_WIDTH, SCREEN_HEIGHT))
+                screen.blit(game_over_img, (0, 0))
+                pygame.mixer.music.stop()
                 round_over = True
 
             # event handler
